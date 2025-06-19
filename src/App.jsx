e9c78220f52ec71781b07092dad111242a52c09a
Aiswarya P <aiswaryap234@gmail.com>
2025-06-19 15:00:53 +0530

    new update app

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello World!!
+          Hello World welcome
         </p>
       </div>
       <p className="read-the-docs">
